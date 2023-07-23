e5775f6a0e5fa18fb9939465ad1403d8bc605e1f
hee <98_han@naver.com>
2023-07-23 20:00:40 +0900

    Feat : Question 관리를 위한 admin 등록

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from board.models import Question
 
-# Register your models here.
+admin.site.register(Question)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+
+admin.site.register(Question, QuestionAdmin)
+
