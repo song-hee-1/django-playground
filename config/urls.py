3aa26fe27f0563fc25118bbae62b4217c3a3ed54
hee <98_han@naver.com>
2023-08-08 09:45:00 +0900

    Feat : views url 분리

--- config/urls.py
+++ config/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from board import views
+
+from views import base_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('board/', include('board.urls')),
     path('accounts/', include('accounts.urls')),
-    path('', views.index, name='index',)
+    path('', base_views.index, name='index',)
 ]
