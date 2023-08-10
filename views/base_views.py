3b2e529f1698101d12ecdbcc7c1938e24f58b1a7
hee <98_han@naver.com>
2023-08-10 20:28:44 +0900

    Feat : 검색 기능

--- views/base_views.py
+++ views/base_views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 
 from board.models import Question
 
@@ -9,12 +10,20 @@ def index(request):
     board 목록 출력
     """
     page = request.GET.get('page', '1')
+    kw = request.GET.get('kw', '')
     question_list = Question.objects.all().order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |
+            Q(content__icontains=kw) |
+            Q(author__username__icontains=kw) |
+            Q(answer__author__username__icontains=kw)
+        ).distinct()
 
     paginator = Paginator(question_list, 10)
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
 
     return render(request, 'board/question_list.html', context)
 
