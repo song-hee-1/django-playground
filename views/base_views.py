21e205e6ffc096fdc631a4232d0e3db8f73e639f
hee <98_han@naver.com>
2023-08-10 20:34:53 +0900

    Feat : 정렬 기능

--- views/base_views.py
+++ views/base_views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from django.db.models import Q
+from django.db.models import Q, Count
 
 from board.models import Question
 
@@ -11,7 +11,15 @@ def index(request):
     """
     page = request.GET.get('page', '1')
     kw = request.GET.get('kw', '')
-    question_list = Question.objects.all().order_by('-create_date')
+    so = request.GET.get('so', 'recent')
+
+    if so == 'recommend':
+        question_list = Question.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    else:  # recent
+        question_list = Question.objects.order_by('-create_date')
+
     if kw:
         question_list = question_list.filter(
             Q(subject__icontains=kw) |
